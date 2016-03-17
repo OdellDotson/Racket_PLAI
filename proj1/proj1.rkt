91083d0dc24dd761901118cc0c06ce950ddb41ee
Odell Dotson <odelldotson@gmail.com>
2016-03-17 17:34:18 -0400

    Finished problems 7 and 8. 8 needs more testing. All hail functional programming.

--- proj1/proj1.rkt
+++ proj1/proj1.rkt
@@ -44,8 +44,75 @@
 
 
 ;Problem 4
-(define (alternating lta)
-	
-)
+;(define (alternating lta)
+;	
+;)
 
-(test (alternating (list 1 2 3 4 5)) (list 1 3 5))
+;(test (alternating (list 1 2 3 4 5)) (list 1 3 5))
+
+
+;; Problem 5
+
+(define-type Scores
+	[scores (midtermGrade : number) (finalGrade : number) (courseGrade : string)])
+
+;; Problem 6
+(define-type Students
+	[undergrad (name : string) (grades : Scores) (gradYear : number)]
+	[graduate (name : string) (grades : Scores) (degreeProg : string)])
+
+;; (Problem 7 : spaghetti)
+(define (assign-grades los)
+	(cond   [(empty? los) empty]
+		[(cons? los) 
+			(map (lambda (student) 
+				(cond   [(undergrad? student) 
+						(undergrad (undergrad-name student) (scores (scores-midtermGrade (undergrad-grades student)) (scores-finalGrade (undergrad-grades student))
+						(cond 	[(> (/ (+ (scores-midtermGrade (undergrad-grades student)) (scores-finalGrade (undergrad-grades student)) ) 2) 85) "high pass"]
+							[(> (/ (+ (scores-midtermGrade (undergrad-grades student)) (scores-finalGrade (undergrad-grades student)) ) 2) 65) "pass"]
+							[else "fail"]
+							)) (undergrad-gradYear student))]
+					[(graduate? student) 
+						(graduate (graduate-name student) (scores (scores-midtermGrade (graduate-grades student)) (scores-finalGrade (graduate-grades student)) 
+						(cond [(> (/ (+ (scores-midtermGrade (graduate-grades student)) (scores-finalGrade (graduate-grades student))) 2) 85) "high pass"]
+                                                      [(> (/ (+ (scores-midtermGrade (graduate-grades student)) (scores-finalGrade (graduate-grades student))) 2) 65) "pass"]
+                                                      [else "fail"])) (graduate-degreeProg student))])
+			)
+			los)]))
+"Problem 7 Tests"	
+(define ugs1 (undergrad "UGS1" (scores 90 95 "A") 2017))
+(define ugs2 (undergrad "UGS2" (scores 85 80 "B") 2019))
+(define gs1 (graduate "GS1" (scores 70 75 "C") "PhD"))
+(define gs2 (graduate "GS2" (scores 35 40 "NR") "MS"))
+
+(define tugs1 (undergrad "UGS1" (scores 90 95 "high pass") 2017))
+(define tugs2 (undergrad "UGS2" (scores 85 80 "pass") 2019))
+(define tgs1 (graduate "GS1" (scores 70 75 "pass") "PhD"))
+(define tgs2 (graduate "GS2" (scores 35 40 "fail") "MS"))
+
+
+
+(test (assign-grades (list ugs1 ugs2 gs1 gs2)) (list tugs1 tugs2 tgs1 tgs2))  
+
+
+
+;; Problem 8
+(define (all-phd-pass? los)
+	(foldl (lambda(a b) (and a b)) #t (map (lambda (student)
+		(cond [(empty? los) #t];If there's no one on the list, everyone on the list passed.
+			[(cons? los) 
+				(cond [(undergrad? student) #t]
+					[(graduate? student) 
+						(cond [(string=? "MS" (graduate-degreeProg student)) #t];true
+							[ (string=? "PhD" (graduate-degreeProg student)) 
+							(cond [(string=? "pass" (scores-courseGrade (graduate-grades student)))#t]
+								[else #f])	
+								])])])) (assign-grades los) )));false
+
+"Problem 8 Tests"
+(test (all-phd-pass? (list gs1 gs2)) #t)
+(test (all-phd-pass? (list gs1)) #t)
+
+
+
+
